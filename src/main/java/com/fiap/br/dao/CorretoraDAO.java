b57c596d11eac299feed7426186aaa7438666547
Rodrigo050705 <135657705+Rodrigo050705@users.noreply.github.com>
2024-10-14 23:06:29 -0300

    adicionando update e delete em corretora

--- src/main/java/com/fiap/br/dao/CorretoraDAO.java
+++ src/main/java/com/fiap/br/dao/CorretoraDAO.java
@@ -31,4 +31,24 @@ public class CorretoraDAO {
         PreparedStatement stm = connection.prepareStatement("select * from corretora where id = ?");
         stm.setString(1, id);
     }
+
+    public void updateCorretora(Corretora corretora) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("update corretora set id_corretora = ?, nome_corretora = ?, cnpj = ?, email = ?, telefone = ?, tipos_criptoativos_suportados = ?, endereco_carteira_corretora = ? where id_corretora = ?");
+        stm.setString(1, corretora.getIdCorretora().toString());
+        stm.setString(2, corretora.getNomeCorretora());
+        stm.setString(3, corretora.getCnpj());
+        stm.setString(4, corretora.getEmail());
+        stm.setString(5, corretora.getTelefone());
+        stm.setString(6, corretora.getTiposCriptoativosSuportados().toString());
+        stm.setString(7, corretora.getEnderecoCarteiraCorretora());
+        stm.executeUpdate();
+    }
+
+    public void deleteCorretora(String id_corretora) throws SQLException{
+        PreparedStatement stm = connection.prepareStatement("delete from corretora where id_corretora = ?");
+        stm.setString(1, id_corretora);
+        int linha = stm.executeUpdate();
+        if (linha == 0)
+            throw new SQLException("Corretora não encontrada para ser removida");
+    }
 }
